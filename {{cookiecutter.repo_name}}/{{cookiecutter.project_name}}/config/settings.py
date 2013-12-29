7cf57dfb0065ce59f76c6f8e7dff2d24726d6e2f
Christopher Knox <chris@dragonfly.co.nz>
2013-12-29 14:39:42 +1300

    changes from actually deploying

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/config/settings.py
@@ -216,7 +216,6 @@ class Common(Configuration):
         'django_browserid.auth.BrowserIDBackend',
     )
 
-    BROWSERID_AUDIENCES = ['http://localhost:8000', 'http://127.0.0.1:8000']
 
     ########## END AUTHENTICATION CONFIGURATION
 
@@ -313,6 +312,10 @@ class Local(Common):
         'SHOW_TEMPLATE_CONTEXT': True,
     }
     ########## end django-debug-toolbar
+    
+    ########## browserid
+    BROWSERID_AUDIENCES = ['http://localhost:8000', 'http://127.0.0.1:8000']
+    ########## end browserid
 
     ########## Your local stuff: Below this line define 3rd party libary settings
 
@@ -327,19 +330,23 @@ class Production(Common):
     SECRET_KEY = values.SecretValue()
     ########## END SECRET KEY
 
-    ########## django-secure
-    INSTALLED_APPS += ("djangosecure", )
+    ########## browserid
+    BROWSERID_AUDIENCES = []
+    ########## end browserid
 
-    # set this to 60 seconds and then to 518400 when you can prove it works
-    SECURE_HSTS_SECONDS = 60
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_FRAME_DENY = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
-    SECURE_BROWERER_XSS_FILTER = values.BooleanValue(True)
-    SESSION_COOKIE_SECURE = values.BooleanValue(False)
-    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
-    ########## end django-secure
+    # ########## django-secure
+    # INSTALLED_APPS += ("djangosecure", )
+
+    # # set this to 60 seconds and then to 518400 when you can prove it works
+    # SECURE_HSTS_SECONDS = 60
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    # SECURE_FRAME_DENY = values.BooleanValue(True)
+    # SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    # SECURE_BROWERER_XSS_FILTER = values.BooleanValue(True)
+    # SESSION_COOKIE_SECURE = values.BooleanValue(False)
+    # SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    # SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    # ########## end django-secure
 
     ########## SITE CONFIGURATION
     # Hosts/domain names that are valid for this site
@@ -349,44 +356,44 @@ class Production(Common):
 
     INSTALLED_APPS += ("gunicorn", )
 
-    ########## STORAGE CONFIGURATION
-    # See: http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += (
-        'storages',
-    )
+    # ########## STORAGE CONFIGURATION
+    # # See: http://django-storages.readthedocs.org/en/latest/index.html
+    # INSTALLED_APPS += (
+    #     'storages',
+    # )
 
-    # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+    # STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-    # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    AWS_ACCESS_KEY_ID = values.SecretValue()
-    AWS_SECRET_ACCESS_KEY = values.SecretValue()
-    AWS_STORAGE_BUCKET_NAME = values.SecretValue()
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
+    # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+    # AWS_ACCESS_KEY_ID = values.SecretValue()
+    # AWS_SECRET_ACCESS_KEY = values.SecretValue()
+    # AWS_STORAGE_BUCKET_NAME = values.SecretValue()
+    # AWS_AUTO_CREATE_BUCKET = True
+    # AWS_QUERYSTRING_AUTH = False
 
-    # AWS cache settings, don't change unless you know what you're doing:
-    AWS_EXPIREY = 60 * 60 * 24 * 7
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY,
-            AWS_EXPIREY)
-    }
+    # # AWS cache settings, don't change unless you know what you're doing:
+    # AWS_EXPIREY = 60 * 60 * 24 * 7
+    # AWS_HEADERS = {
+    #     'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY,
+    #         AWS_EXPIREY)
+    # }
 
-    # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-    STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-    ########## END STORAGE CONFIGURATION
+    # # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+    # STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+    # ########## END STORAGE CONFIGURATION
 
-    ########## EMAIL
-    DEFAULT_FROM_EMAIL = values.Value(
-            '{{cookiecutter.project_name}} <{{cookiecutter.project_name}}-noreply@{{cookiecutter.domain_name}}>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[{{cookiecutter.project_name}}] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
-    SERVER_EMAIL = EMAIL_HOST_USER
-    ########## END EMAIL
+    # ########## EMAIL
+    # DEFAULT_FROM_EMAIL = values.Value(
+    #         '{{cookiecutter.project_name}} <{{cookiecutter.project_name}}-noreply@{{cookiecutter.domain_name}}>')
+    # EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    # EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    # EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    # EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    # EMAIL_SUBJECT_PREFIX = values.Value('[{{cookiecutter.project_name}}] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    # EMAIL_USE_TLS = True
+    # SERVER_EMAIL = EMAIL_HOST_USER
+    # ########## END EMAIL
 
     ########## TEMPLATE CONFIGURATION
 
